750e2dce563593be3aeb96bba9e92541383f0d3b
ayunas <ayunas@gmail.com>
2019-05-14 17:20:44 -0500

    added zoos to the database and retrieving them with a .get('/zoos')

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const server = express();
+const knex = require('knex');
 const router = express.Router();
+
+const knexConfig = {
+  client : 'sqlite3',
+  connection : {
+    filename : './data/zoos.sqlite3'
+  },
+  useNullAsDefault : true,
+  debug : true
+}
+
+const db = knex(knexConfig);
 
 server.use(express.json());
 server.use(helmet());
@@ -10,11 +21,24 @@ server.use('/zoos', router);
 
 server.get('/', (req,res) => {
   res.send('the server is working');
+
 } )
 
 router.get('/', (req,res) => {
-  res.json({message : 'the router is working'})
-})
+  //res.json({message : 'the router is working'})
+  db('zoos')
+    .then( animals => {
+      res.status(200).json(animals)
+    })
+    .catch( err => {
+      res.status(500).json(err.message);
+    })
+});
+
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
