e4858dfba314fbba334565f25faa01900aa619a1
ayunas <ayunas@gmail.com>
2019-05-14 16:49:54 -0500

    set up project and set up server and router to '/zoos'

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 
 const server = express();
+const router = express.Router();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/zoos', router);
 
-// endpoints here
+server.get('/', (req,res) => {
+  res.send('the server is working');
+} )
+
+router.get('/', (req,res) => {
+  res.json({message : 'the router is working'})
+})
 
 const port = 3300;
 server.listen(port, function() {
